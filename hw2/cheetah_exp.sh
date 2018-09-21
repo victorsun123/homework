39dbc4a72d4bb21a14fc579446e36a9d4ee3d4c9
Victor <victorsun@berkeley.edu>
2018-09-21 01:15:08 -0700

    add scripts hw2

---
+++ hw2/cheetah_exp.sh
@@ -0,0 +1,15 @@
+python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.9 -n 100 -e 3 -l 2 -s 32 -b 10000 -lr .005 -rtg --nn_baseline --exp_name hc_b10000_r.005
+python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.9 -n 100 -e 3 -l 2 -s 32 -b 10000 -lr .01 -rtg --nn_baseline --exp_name hc_b10000_r.01
+python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.9 -n 100 -e 3 -l 2 -s 32 -b 10000 -lr .02 -rtg --nn_baseline --exp_name hc_b10000_r.02
+python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.9 -n 100 -e 3 -l 2 -s 32 -b 30000 -lr .005 -rtg --nn_baseline --exp_name hc_b30000_r.005
+python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.9 -n 100 -e 3 -l 2 -s 32 -b 30000 -lr .01 -rtg --nn_baseline --exp_name hc_b30000_r.01
+python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.9 -n 100 -e 3 -l 2 -s 32 -b 30000 -lr .02 -rtg --nn_baseline --exp_name hc_b30000_r.02
+python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.9 -n 100 -e 3 -l 2 -s 32 -b 50000 -lr .005 -rtg --nn_baseline --exp_name hc_b50000_r.005
+python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.9 -n 100 -e 3 -l 2 -s 32 -b 50000 -lr .01 -rtg --nn_baseline --exp_name hc_b50000_r.01
+python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.9 -n 100 -e 3 -l 2 -s 32 -b 50000 -lr .02 -rtg --nn_baseline --exp_name hc_b50000_r.02
+
+
+
+
+
+
